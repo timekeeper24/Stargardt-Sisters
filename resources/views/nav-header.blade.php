02a0bf8fe8be605b97c2022070fcc9ed0720021d
johndeluce <johnrdeluce@gmail.com>
2024-06-10 17:40:11 -0400

    simple nav bar and logo button

---
+++ resources/views/nav-header.blade.php
@@ -0,0 +1,19 @@
+
+
+
+<link rel="stylesheet" href="{{ asset('assets/css/nav.css') }}">
+
+
+<header class="nav-header">
+    <!-- Logo -->
+    <h1><a class="logo" href="{{ url('/') }}">Stargardt Sisters</a></h1>
+
+    <!-- Nav Bar -->
+    <nav>
+        <a class="nav-button" href="https://www.fightingblindness.org/resources/the-stargardt-sisters-258">About</a>
+        <a class="nav-button">Gallery</a>
+        <a class="nav-button">News</a>
+        <a class="nav-button">Contact</a>
+    </nav>
+
+</header>
