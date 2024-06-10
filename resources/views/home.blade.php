d75418bd749675709bb078b2377319dad0a15a66
jamespeachh <matthlewis24@gmail.com>
2024-06-10 11:17:55 -0400

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stargardt Sisters</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/home.css') }}">
+</head>
+<body>
+    <div class="home-wrapper">
+
+        <div class="main-section-of-home">
+            <header class="home-header">
+                <h1>Stargardt Sisters</h1>
+            </header>
+            <div class="video-wrapper">
+                <span class="border-extend"></span>
+                <video controls>
+                    <source src="{{ asset('assets/video_name.mp4') }}" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
