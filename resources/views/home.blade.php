1e4b7a1734c67bb06ed8b2f3ae72e6d8ea6b774b
jamespeachh <matthlewis24@gmail.com>
2024-06-10 13:39:03 -0400

    Fixing some weird bugs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,20 +7,20 @@
     <link rel="stylesheet" href="{{ asset('assets/css/home.css') }}">
 </head>
 <body>
-    <div class="home-wrapper">
+<div class="home-wrapper">
 
-        <div class="main-section-of-home">
-            <header class="home-header">
-                <h1>Stargardt Sisters</h1>
-            </header>
-            <div class="video-wrapper">
-                <span class="border-extend"></span>
-                <video controls>
-                    <source src="{{ asset('assets/video_name.mp4') }}" type="video/mp4">
-                    Your browser does not support the video tag.
-                </video>
-            </div>
+    <div class="main-section-of-home">
+        <header class="home-header">
+            <h1>Stargardt Sisters</h1>
+        </header>
+        <div class="video-wrapper">
+            <span class="border-extend"></span>
+            <video controls>
+                <source src="{{ asset('assets/video_name.mp4') }}" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
         </div>
     </div>
+</div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,4 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', [HomeController::class, 'index'])->name('home');
