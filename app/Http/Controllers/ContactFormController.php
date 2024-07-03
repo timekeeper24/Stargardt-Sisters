ccb1f2b410e524508c12352204006788c06aca35
jamespeachh <matthlewis24@gmail.com>
2024-07-03 14:09:46 -0400

    WIP on contact-form

---
+++ app/Http/Controllers/ContactFormController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactFormController extends Controller
+{
+    public function index()
+    {
+        
+    }
+
+    public function submit(Request $request)
+    {
+
+    }
+}
